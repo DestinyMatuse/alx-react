b7effb1d6bf881e552f5d09c6c95b27c45970aa0
DestinyMatuse <133487921+DestinyMatuse@users.noreply.github.com>
2024-06-03 20:49:37 +0200

    Add files via upload

--- 0x04-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,40 +1,93 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import './Notifications.css';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropeTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
 
-class NotificationItem extends React.PureComponent {
+class Notifications extends React.Component {
   constructor(props) {
     super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
   }
 
   render() {
     return (
-      this.props.value ? 
-      <li
-      data-notification-type={this.props.type}
-      onClick={() => this.props.markAsRead(this.props.id)}
-      >{this.props.value}</li> 
-      :
-      <li
-      data-notification-type={this.props.type}
-      dangerouslySetInnerHTML={this.props.html}
-      onClick={() => {console.log('empty func');}}
-      ></li>
+      <>
+        <div className='menuItem'>
+          Your notifications
+        </div>
+        {this.props.displayDrawer? 
+            <div className="Notifications">
+              <button style={{
+                color: '#3a3a3a',
+                fontWeight: 'bold',
+                background: 'none',
+                border: 'none',
+                fontSize: '15px',
+                position: 'absolute',
+                right: '3px',
+                top: '3px',
+                cursor: 'pointer',
+                outline: 'none',
+              }}
+              aria-label="Close"
+              onClick={(e) => {
+                console.log('Close button has been clicked');
+              }}
+              >
+                <img src={closeIcon} alt="close icon" width="15px" />
+              </button>
+              {
+                this.props.listNotifications.length != 0 ?
+                  <p>Here is the list of notifications</p>
+                : null
+              }
+              <ul>
+                {
+                  this.props.listNotifications.length == 0 ?
+                    <NotificationItem type="default" value="No new notification for now" />
+                  : null
+                }
+                {
+                  this.props.listNotifications.map((val, idx)=> {
+                    return <NotificationItem
+                    type={val.type}
+                    value={val.value}
+                    html={val.html}
+                    key={val.id}
+                    markAsRead={this.markAsRead}
+                    id={val.id}
+                  />
+                  })
+                }
+              </ul>
+            </div>
+          :
+            null
+        }
+        
+      </>
     );
   }
 }
 
-NotificationItem.defaultProps = {
-  type: 'default',
-  markAsRead: () => {console.log('empty func');},
-	id: 0
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: []
 };
 
-NotificationItem.propTypes = {
-  html: PropTypes.shape({__html: PropTypes.string}),
-  type: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  markAsRead: PropTypes.func,
-  id: PropTypes.number
+Notifications.propTypes = {
+  displayDrawer: PropeTypes.bool,
+  listNotifications: PropeTypes.arrayOf(NotificationItemShape)
 };
 
-export default NotificationItem;
+export default Notifications;
